448575471d9a4439ba12ecc7f6c4cfddbe63f075
Ylvi ELEZAJ <yelez@lmdsio.com>
2024-09-18 14:42:44 +0200

    Mon projet version 2

--- accueil.php
+++ accueil.php
@@ -1,1 +1,13 @@
 Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+if ($_POST["login"]=="admin" && $_POST["mdp"]=="azerty") {
+    $extra = 'profil.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}else{
+    $extra = 'index.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}
